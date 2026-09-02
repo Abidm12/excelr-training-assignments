0ca0bd551f59932a6958dc79c4f6b7406983f0fa
Abidm12 <abidmaner12@gmail.com>
2026-09-02 09:40:01 +0530

    Day 3 Assignments

---
+++ java/Day3/Question1.java
@@ -0,0 +1,26 @@
+/*
+1)Write a program that reads a four-digit integer from the user and prints the sum
+of its digits (e.g., input 1234 should print 10). You must use only arithmetic
+operators.
+*/
+
+package java.Day3;
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a four-digit number: ");
+        int num = sc.nextInt();
+
+        int d1 = num / 1000;
+        int d2 = (num / 100) % 10;
+        int d3 = (num / 10) % 10;
+        int d4 = num % 10;
+
+        int sum = d1 + d2 + d3 + d4;
+
+        System.out.println(sum);
+    }
+}
